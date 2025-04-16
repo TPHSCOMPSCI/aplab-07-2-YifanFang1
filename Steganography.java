bae03094fb6a59ec9be3fdbf99681f741d5ce34e
YifanFang1 <148043595+YifanFang1@users.noreply.github.com>
2025-04-16 12:59:20 -0700

    finally done

--- Steganography.java
+++ Steganography.java
@@ -6,69 +6,69 @@ import java.util.Scanner;
 public class Steganography {
     public static void main(String[] args) {
         Picture beach = new Picture ("beach.jpg");
-        // beach.explore();
+        beach.explore();
 
-        // Picture copy = testClearLow(beach);
-        // copy.explore();
+        Picture copy = testClearLow(beach);
+        copy.explore();
 
-        // Picture copy2 = testSetLow(beach, Color.PINK);
-        // copy2.explore();
+        Picture copy2 = testSetLow(beach, Color.PINK);
+        copy2.explore();
 
-        // Picture copy3 = revealPicture(copy2);
-        // copy3.explore();
+        Picture copy3 = revealPicture(copy2);
+        copy3.explore();
 
-        // Picture moon = new Picture("moon-surface.jpg");
-        // if (canHide(copy3, moon)) {
-        //     Picture copy4 = hidePicture(copy3, moon, 0, 0);
-        //     copy4.explore();
-        // }
+        Picture moon = new Picture("moon-surface.jpg");
+        if (canHide(copy3, moon)) {
+            Picture copy4 = hidePicture(copy3, moon, 0, 0);
+            copy4.explore();
+        }
         
-        // Picture robot = new Picture("robot.jpg"); 
-        // Picture flower1 = new Picture("flower1.jpg");
-        // beach.explore();
-        // Picture hidden1 = hidePicture(beach, robot, 65, 208); 
-        // Picture hidden2 = hidePicture(hidden1, flower1, 280, 110); 
-        // hidden2.explore(); 
-        // Picture unhidden = revealPicture(hidden2); 
-        // unhidden.explore(); 
+        Picture robot = new Picture("robot.jpg"); 
+        Picture flower1 = new Picture("flower1.jpg");
+        beach.explore();
+        Picture hidden1 = hidePicture(beach, robot, 65, 208); 
+        Picture hidden2 = hidePicture(hidden1, flower1, 280, 110); 
+        hidden2.explore(); 
+        Picture unhidden = revealPicture(hidden2); 
+        unhidden.explore(); 
 
-        // Picture swan = new Picture("swan.jpg"); 
-        // Picture swan2 = new Picture("swan.jpg"); 
-        // System.out.println("Swan and swan2 are the same: " + isSame(swan, swan2)); 
-        // swan = testClearLow(swan); 
-        // System.out.println("Swan and swan2 are the same (after clearLow run on swan): " + isSame(swan, swan2));
+        Picture swan = new Picture("swan.jpg"); 
+        Picture swan2 = new Picture("swan.jpg"); 
+        System.out.println("Swan and swan2 are the same: " + isSame(swan, swan2)); 
+        swan = testClearLow(swan); 
+        System.out.println("Swan and swan2 are the same (after clearLow run on swan): " + isSame(swan, swan2));
         
         
-        // Picture arch = new Picture("arch.jpg"); 
-        // Picture arch2 = new Picture("arch.jpg");
-        // Picture koala = new Picture("koala.jpg"); 
-        // Picture robot1 = new Picture("robot.jpg");
-        // ArrayList<Point> pointList = findDifferences(arch, arch2); 
-        // System.out.println("PointList after comparing two identical has a size of " + pointList.size()); 
-        // pointList = findDifferences(arch, koala); 
-        // System.out.println("PointList after comparing two different sized pictures has a size of " + pointList.size()); 
-        // arch2 = hidePicture(arch, robot1, 65, 102); 
-        // pointList = findDifferences(arch, arch2);
-        // System.out.println("Pointlist after hiding a picture has a size of " + pointList.size()); 
-        // arch.show(); 
-        // arch2.show(); 
+        Picture arch = new Picture("arch.jpg"); 
+        Picture arch2 = new Picture("arch.jpg");
+        Picture koala = new Picture("koala.jpg"); 
+        Picture robot1 = new Picture("robot.jpg");
+        ArrayList<Point> pointList = findDifferences(arch, arch2); 
+        System.out.println("PointList after comparing two identical has a size of " + pointList.size()); 
+        pointList = findDifferences(arch, koala); 
+        System.out.println("PointList after comparing two different sized pictures has a size of " + pointList.size()); 
+        arch2 = hidePicture(arch, robot1, 65, 102); 
+        pointList = findDifferences(arch, arch2);
+        System.out.println("Pointlist after hiding a picture has a size of " + pointList.size()); 
+        arch.show(); 
+        arch2.show(); 
 
-        // Picture hall = new Picture("femaleLionAndHall.jpg"); 
-        // Picture robot2 = new Picture("robot.jpg"); 
-        // Picture flower2 = new Picture("flower1.jpg"); 
-        // Picture hall2 = hidePicture(hall, robot2, 50, 300); 
-        // Picture hall3 = hidePicture(hall2, flower2, 115, 275); 
-        // hall3.explore(); 
-        // if(!isSame(hall, hall3)) { 
-        //     Picture hall4 = showDifferentArea(hall, findDifferences(hall, hall3)); 
-        //     hall4.show();  
-        //     Picture unhiddenHall3 = revealPicture(hall3);
-        //     unhiddenHall3.show(); 
-        // } 
+        Picture hall = new Picture("femaleLionAndHall.jpg"); 
+        Picture robot2 = new Picture("robot.jpg"); 
+        Picture flower2 = new Picture("flower1.jpg"); 
+        Picture hall2 = hidePicture(hall, robot2, 50, 300); 
+        Picture hall3 = hidePicture(hall2, flower2, 115, 275); 
+        hall3.explore(); 
+        if(!isSame(hall, hall3)) { 
+            Picture hall4 = showDifferentArea(hall, findDifferences(hall, hall3)); 
+            hall4.show();  
+            Picture unhiddenHall3 = revealPicture(hall3);
+            unhiddenHall3.show(); 
+        } 
 
-        // beach.explore();
+        beach.explore();
         Picture p = hideText(beach, "HELLO WORLD");
-        // p.explore();
+        p.explore();
         System.out.println(revealText(p));
     }
 
@@ -225,7 +225,7 @@ public class Steganography {
     private static int[] getBitPairs(int num) { 
         int[] bits = new int[3];   
         int code = num;   
-        for (int i = 0; i < 2; i++)   { 
+        for (int i = 0; i < 3; i++)   { 
             bits[i] = code % 4; 
             code = code / 4;
         }  
@@ -237,7 +237,8 @@ public class Steganography {
 
     public static Picture hideText(Picture source, String s) {
         ArrayList<Integer> list = encodeString(s);
-        ArrayList<int[]> bits = new ArrayList<int[]>();
+        ArrayList<int[]> bits = new ArrayList<>();
+
         for(int num : list) {
             // System.out.println(getBitPairs(num)[0] + " " + getBitPairs(num)[1] + " " + getBitPairs(num)[2]);
             bits.add(getBitPairs(num));
@@ -246,10 +247,12 @@ public class Steganography {
         Picture pic = new Picture(source);
         Pixel[][] px = pic.getPixels2D();
         System.out.println();
+        
+        int j = 0;
+        int k = 0;
 
         for(int i = 0; i < bits.size(); i++) {
-            int j = 0;
-            int k = 0;
+            
 
             Pixel p = px[j][k];
 
@@ -265,13 +268,8 @@ public class Steganography {
             p.setGreen(((p.getGreen()/4)*4)    +   bits.get(i)[1]);
             p.setBlue(((p.getBlue()/4)*4)  +   bits.get(i)[2]);
 
-            System.out.println(bits.get(i)[0] + " " + bits.get(i)[1] + " " + bits.get(i)[2]);
-
-            System.out.println();
-
-            System.out.println(p.getRed() + " " + p.getGreen() + " " + p.getBlue());
-
-            System.out.println();
+            // System.out.println(bits.get(i)[0] + " " + bits.get(i)[1] + " " + bits.get(i)[2] + "\n");
+            // System.out.println(p.getRed() + " " + p.getGreen() + " " + p.getBlue() + "\n");
 
             k++;
 
@@ -296,17 +294,17 @@ public class Steganography {
             for (int b = 0; b < px[0].length; b++) {
                 Color col = px[a][b].getColor();
                 
-                System.out.println(col.getRed() + " " + col.getGreen() + " " + col.getBlue() + "\n");
+                // System.out.println(col.getRed() + " " + col.getGreen() + " " + col.getBlue() + "\n");
+                //System.out.println(col.getRed()%4 + " " + col.getGreen()%4 + " " + col.getBlue()%4 + "\n");
 
-                System.out.println(col.getRed()%4 + " " + col.getGreen()%4 + " " + col.getBlue()%4 + "\n");
-
-                int numS = col.getRed()%4 + col.getGreen()%4 + col.getBlue()%4;
+                int numS = col.getRed()%4 + col.getGreen()%4*4 + col.getBlue()%4*16;
                 
-                System.out.println(numS);
+                System.out.println("value: " + numS + "\n");
                 
                 if(numS == 0) {
                     return decodeString(resultArray);
                 }
+                
                 resultArray.add(numS);
             }
         }
